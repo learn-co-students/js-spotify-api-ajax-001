76ce5858d3150d17342ab9c4ee413f2e96fbdfd8
Garett Arrowood <garettarrowood@gmail.com>
2015-11-30 09:52:58 -0500

    Done.

--- js/spotify-chart.js
+++ js/spotify-chart.js
@@ -13,29 +13,38 @@ $(function() {
 });
 
 function extractTop20Tracks(tracks) {
-  // your code here
+  return tracks.slice(0,20);
 }
 
 function extractNumberOfStreams(tracks) {
-  // your code here
+  numArray = [];
+  for(var i=0;i<tracks.length;i++){
+    numArray.push(tracks[i].num_streams);
+  }
+  return numArray;
 }
 
 function extractNames(tracks) {
-  // your code here
+  nameArray = [];
+  for(var i=0;i<tracks.length;i++){
+    nameArray.push(tracks[i].track_name);
+  }
+  return nameArray;
 }
 
 function chartData(labels, inputData) {
-  // your code here
-
-  // use the dataSetProperties variable defined above if it helps
+  dataSetProperties.data = inputData;
+  return { labels: labels, datasets: [dataSetProperties] };
 }
 
 function getSpotifyTracks(callback){
-  // your ajax call here, on success it should call on the 
-  // parameter it's passed (it's a function), and pass it's 
-  // parameter the data it received
-
-  // use the url variable defined above if it helps
+  $.ajax({
+    url: url,
+    dataType: 'jsonp',
+    success: function(response){
+      callback(response);
+    }
+  })
 }
 
 function success(parsedJSON) {
@@ -43,10 +52,16 @@ function success(parsedJSON) {
   // http://www.chartjs.org/docs/#bar-chart
   // you will need to call on:
   //  1. extractTop20Tracks - pass it tracks
+  var tracks = extractTop20Tracks(parsedJSON.tracks);
   //  2. extractNames -  pass it the result of #1
+  var labels = extractNames(tracks);
   //  3. extractNumberOfStreams - pass it the result of #1
+  var streams = extractNumberOfStreams(tracks);
   //  4. chartData - pass it results of #2 and #3
+  var data = chartData(labels, streams);
   //  5. make a variable `ctx` and select the canvas with the id of spotify-chart
   //     * also make sure to specify 2d context
+  var ctx = $('#spotify-chart')[0].getContext('2d');
   //  6. make a new bar chart!
+  return new Chart(ctx).Bar(data, {});
 }
--- spec/spotify-chart.spec.js
+++ spec/spotify-chart.spec.js
@@ -21,8 +21,14 @@ describe("spotifyChart", function(){
   });
 
   describe("#chartData", function(){
-    it("returns an object where the attribute `labels` points to the first param, the attribute `datasets` points to an array of objects, the first of which hassix attributes, 1) label 2) fillColor 3) strokeColor 4) highlightFill 5) highlightStroke and 6) data, data points to the second param", function(){
-      var expected = { labels : [ 'label1', 'label2' ], datasets : [ { label : 'Spotify Chart of Top 20 Streamed Songs on Spotify with their Steam Count', fillColor : 'rgba(220,220,220,0.5)', strokeColor : 'rgba(220,220,220,0.8)', highlightFill : 'rgba(220,220,220,0.75)', highlightStroke : 'rgba(220,220,220,1)', data : [ 'data1', 'data2' ] } ] };
+    it("returns an object where the attribute `labels` points to the first param, the attribute `datasets` points to an array of objects, the first of which has six attributes, 1) label 2) fillColor 3) strokeColor 4) highlightFill 5) highlightStroke and 6) data, data points to the second param", function(){
+      var expected = { labels : [ 'label1', 'label2' ], 
+          datasets : [ { label : 'Spotify Chart of Top 20 Streamed Songs on Spotify with their Steam Count', 
+          fillColor : 'rgba(220,220,220,0.5)', 
+          strokeColor : 'rgba(220,220,220,0.8)', 
+          highlightFill : 'rgba(220,220,220,0.75)', 
+          highlightStroke : 'rgba(220,220,220,1)', 
+          data : [ 'data1', 'data2' ] } ] };
       expect(chartData(['label1', 'label2'], ['data1', 'data2'])).toEqual(expected);
     });
   });
@@ -42,7 +48,7 @@ describe("spotifyChart", function(){
   describe("ran server and checked that chart rendered", function(){
     it("looks similar to the picture in the readme and I'm done with the lab", function() {
       // type your name in the empty string below
-      var signature = "";
+      var signature = "Garett Arrowood";
       expect(signature.length).toBeGreaterThan(4);
       var names = signature.split(" ");
       expect(names.length).toBeGreaterThan(1);
