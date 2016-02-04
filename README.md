0a08a1c8a71ddc7354d0a27a2420ef03a1350580
Carleton DiLeo <cbdileo@gmail.com>
2016-02-04 10:39:50 -0800

    Now using new Spotify Top Tracks for artist instead of Top Tracks on Spotify

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 Build a page that uses data from the [Spotify's API Top Tracks](https://developer.spotify.com/web-api/get-artists-top-tracks/) to render a bar chart using [Chart.js](http://chartkick.com/). It will display the 10 most popular Elvis Presley songs and will have track names on the x-axis and popularity on the y-axis.
 
-![example pic](https://github.com/learn-co-curriculum/js-apis-lab/img/example.png "Pic of Example")
+![example pic](https://raw.githubusercontent.com/learn-co-curriculum/js-spotify-api-ajax/cd/new_solution/img/example.png "Pic of Example")
 
 ## Instructions
 
@@ -19,5 +19,3 @@ The final step of this lab will be to run your server with `ruby -run -e httpd .
 * [Spotify's API Top Tracks](https://developer.spotify.com/web-api/get-artists-top-tracks/)
 * [jQuery](http://api.jquery.com/) - [AJAX](http://api.jquery.com/jquery.ajax/)
 * [Chart.js](http://www.chartjs.org/docs/#getting-started) - [Bar Chart](http://www.chartjs.org/docs/#bar-chart-example-usage)
-
-<p data-visibility='hidden'>View <a href='https://learn.co/lessons/js-spotify-api-ajax' title='Visualizing Spotify's Popular Genres'>Visualizing Spotify's Popular Genres</a> on Learn.co and start learning to code for free.</p>
--- js/spotify-chart.js
+++ js/spotify-chart.js
@@ -1,5 +1,12 @@
 var url = "https://api.spotify.com/v1/artists/43ZHCT0cAZBISjO8DG9PnE/top-tracks?country=SE";
 
+var dataSetProperties = {
+  fillColor: 'rgba(220,220,220,0.5)', 
+  strokeColor: 'rgba(220,220,220,0.8)', 
+  highlightFill: 'rgba(220,220,220,0.75)', 
+  highlightStroke: 'rgba(220,220,220,1)'
+};
+
 $(function() {
   getSpotifyTracks(success);
 });
@@ -9,56 +16,40 @@ $(function() {
 // and display the chart correctly in index.html
 
 function extractTop10Tracks(tracks) {
-  return tracks.slice(0, 10);
+  // your code here
 }
 
 function extractPopularity(tracks) {
-  var numOfStreams = [];
-  for (i = 0; i < tracks.length; i++) {
-    numOfStreams.push(tracks[i].popularity);
-  }
-  return numOfStreams;
+  // your code here
 }
 
 function extractNames(tracks) {
-  var names = [];
-  for (i = 0; i < tracks.length; i++) {
-    names.push(tracks[i].name);
-  }
-  return names;
+  // your code here
 }
 
 function chartData(labels, inputData) {
-  var dataObj = {};
-  dataObj.labels = labels;
-  dataObj.datasets = [
-    {
-      fillColor: 'rgba(220,220,220,0.5)', 
-      strokeColor: 'rgba(220,220,220,0.8)', 
-      highlightFill: 'rgba(220,220,220,0.75)', 
-      highlightStroke: 'rgba(220,220,220,1)', 
-      data: inputData
-    }
-  ];
-  return dataObj;
+  // your code here
+
+  // use the dataSetProperties variable defined above if it helps
 }
 
 function getSpotifyTracks(callback){
-  $.ajax({
-    url: url,
-    success: function(result) {
-      callback(result);
-    }
-  });
+  // your ajax call here, on success it should call on the 
+  // parameter it's passed (it's a function), and pass it's 
+  // parameter the data it received
+
+  // use the url variable defined above if it helps
 }
 
 function success(parsedJSON) {
-  var tracks = extractTop10Tracks(parsedJSON.tracks);
-  var names = extractNames(tracks);
-  var streams = extractPopularity(tracks);
-  var data = chartData(names, streams);
-  var ctx = document.getElementById("spotify-chart").getContext("2d");
-  new Chart(ctx).Bar(data);
+  // this function will make a new bar chart, refer to this url:
+  // http://www.chartjs.org/docs/#bar-chart
+  // you will need to call on:
+  //  1. extractTop20Tracks - pass it tracks
+  //  2. extractNames -  pass it the result of #1
+  //  3. extractNumberOfStreams - pass it the result of #1
+  //  4. chartData - pass it results of #2 and #3
+  //  5. make a variable `ctx` and select the canvas with the id of spotify-chart
+  //     * also make sure to specify 2d context
+  //  6. make a new bar chart!
 }
-
-
